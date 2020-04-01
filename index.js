92d6a84b0dd8511ed340b104ae90f6823f365c9b
PacLeonardo97 <you@example.com>
2020-04-01 20:35:07 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const pt_BR = require("date-fns/locale/pt-BR");
+const { utcToZonedTime } = require("date-fns-tz");
+
+const formatDateTimeZone = date => {
+  const parsedDate = utcToZonedTime(parseISO(`${date}Z`), "America/Fortaleza");
+  return format(parsedDate, "dd/MM/yyyy HH:mm:ss");
+};
+
+const formatDateTimeZoneWithPreposition = date => {
+  if (date) {
+    const parsedDate = utcToZonedTime(
+      parseISO(`${date}Z`),
+      "America/Fortaleza"
+    );
+    return format(parsedDate, "dd 'de' MMMM 'de' yyyy HH:mm:ss", {
+      locale: pt_BR
+    });
+  }
+  return date;
+};
+
+const nameThreePoints = (nome, length) => {
+  if (nome.length > 14) {
+    const nomeformatado = nome.substring(0, length) + "...";
+    return nomeformatado;
+  }
+  return nome;
+};
+
+const containsSpecialChar = value => {
+  const pattern = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ 0-9_-]{1,}$/;
+  return !pattern.test(value);
+};
+
+const formatPhoneNumber = str => {
+  const cleaned = ("" + str).replace(/\D/g, "");
+  const match = cleaned.match(/^(\d{2})(\d{4})(\d{5})$/);
+
+  if (!match) return;
+
+  return `(${match[1]}) ${match[2]}-${match[3]}`;
+};
+
+const formatCurrecy = value =>
+  value.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+
+module.exports = {
+  formatDateTimeZone,
+  formatDateTimeZoneWithPreposition,
+  nameThreePoints,
+  containsSpecialChar,
+  formatPhoneNumber,
+  formatCurrecy
+};
